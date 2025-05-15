7b383724592d72de2e40c8e6c81443ffa066fe90
naaadang <naaadang@naver.com>
2025-05-15 15:32:20 +0900

    prometheus, grafana 설정

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -3,8 +3,11 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from pydantic import BaseModel
 import json
 import os
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
+# Prometheus 메트릭스 엔드포인트 (/metrics)
+Instrumentator().instrument(app).expose(app, endpoint="/metrics")
 
 # To-Do 항목을 위한 데이터 모델
 class TodoItem(BaseModel):
